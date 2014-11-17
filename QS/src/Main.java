2e3c9bf40ecb504cfba5a89ce14d768b902c4d83
Andreacmj <andreas.cm.johansson@gmail.com>
2014-11-17 15:39:00 +0100

    Imported BigInteger

--- QS/src/Main.java
+++ QS/src/Main.java
@@ -1,5 +1,14 @@
 /**
  * Created by Andreas on 2014-11-17.
  */
+import java.math.BigInteger;
+
 public class Main {
+
+    public static void main(String[] args) {
+
+
+        BigInteger N = new BigInteger("100000");
+
+    }
 }
