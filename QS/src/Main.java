0083dbd29cdb4a43ca99dd6bf57b92a035249853
Andreacmj <andreas.cm.johansson@gmail.com>
2014-11-17 15:47:37 +0100

    Added primeloop

--- QS/src/Main.java
+++ QS/src/Main.java
@@ -2,6 +2,7 @@
  * Created by Andreas on 2014-11-17.
  */
 import java.math.BigInteger;
+import java.util.LinkedList;
 
 public class Main {
 
@@ -12,7 +13,16 @@ public class Main {
 
         BigInteger B = calcBase(N);
 
+        LinkedList<BigInteger> legendrePrimes = new LinkedList<BigInteger>();
+        long[] primes = Primes.getPrimes();
+        for (int i = 0; i < primes.length; i++) {
+            BigInteger p = new BigInteger(String.valueOf(primes[i]));
+            if (B.compareTo(p) > 0)
+                break;
 
+            if (BigMath.legendre(B, p) == 1)
+                legendrePrimes.add(p);
+        }
 
     }
 
--- QS/src/Primes.java
+++ QS/src/Primes.java
@@ -1,5 +1,12 @@
+import java.math.BigInteger;
+
 /**
  * Created by Andreas on 2014-11-17.
  */
+
 public class Primes {
+
+    static long[] getPrimes() {
+        return new long[] {2, 3, 5};
+    }
 }
